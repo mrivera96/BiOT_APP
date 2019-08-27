768269afd1b44a4298a54622e6d7c63277de6f07
Melvin Josué Rivera Ponce <jylrivera96@gmail.com>
2019-08-27 11:16:10 -0600

    Nuevo SplashScreen

--- app/src/main/java/com/diadema/biometriciot/SplashScreen.java
+++ app/src/main/java/com/diadema/biometriciot/SplashScreen.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.diadema.biometriciot.services.TokenManager;
+
 /**
  * Created by César Andrade on 29/04/2019.
  */
@@ -18,13 +20,22 @@ public class SplashScreen extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         //3000=3 segundos
         int loading = 3000;
+        TokenManager tokenManager = TokenManager.getInstance(getApplicationContext().getSharedPreferences("prefs", MODE_PRIVATE));
+        String token=tokenManager.getToken().getAccessToken();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 //setelah loading maka akan langsung berpindah ke home activity
-                Intent home = new Intent(SplashScreen.this.getApplicationContext(), MainActivity.class);
-                SplashScreen.this.startActivity(home);
-                SplashScreen.this.finish();
+                if(token==null){
+                    Intent login = new Intent(SplashScreen.this.getApplicationContext(), LoginActivity.class);
+                    SplashScreen.this.startActivity(login);
+                    SplashScreen.this.finish();
+                }else{
+                    Intent home = new Intent(SplashScreen.this.getApplicationContext(), MainActivity.class);
+                    SplashScreen.this.startActivity(home);
+                    SplashScreen.this.finish();
+                }
+
             }
         }, loading);
     }
